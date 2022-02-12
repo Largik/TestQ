c1b06265b238731ef2ad0a3878ec8130bab8524e
Largik <57515916+Largik@users.noreply.github.com>
2022-02-12 14:57:46 +0400

    Alpha 1.0

--- app/src/main/java/com/test/project/DetailActivity.kt
+++ app/src/main/java/com/test/project/DetailActivity.kt
@@ -3,9 +3,17 @@ package com.test.project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import io.realm.Realm
+import org.json.JSONObject
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,14 +27,18 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val button = findViewById<Button>(R.id.buttonId)
-        setText()
+
+        setText(button)
 
         setupActionBar()
+
+        val queue = Volley.newRequestQueue(this)
+        getImageFromServer(queue)
 
         button.setOnClickListener{
             val cat = Cat()
             cat.text = intent?.extras?.getString(CAT_FACT_TEXT_TAG).toString()
-            saveIntoDB(cat)
+            requestIntoDB(cat, button)
         }
     }
 
@@ -39,21 +51,65 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun getImageFromServer(queue: RequestQueue) {
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            urlImg,
+            { response ->
+                takeImageUrl(response)
+            },
+            {
+                Toast.makeText(this, "Loading Error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(stringRequest)
+    }
+
+    private fun takeImageUrl(json: String) {
+        val file = JSONObject(json)
+        val urlText = file.getString("file")
+
+        val imageView = findViewById<ImageView>(R.id.imageViewId)
+
+        Glide.with(this).load(urlText).into(imageView)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
     }
 
-    private fun saveIntoDB(cat: Cat){
+    private fun requestIntoDB(cat: Cat, button: Button){
         val realm = Realm.getDefaultInstance()
 
         realm.beginTransaction()
-        realm.copyToRealm(cat)
+        var change = realm.where(Cat::class.java).equalTo("text", cat.text).findFirst()
+        if (change?.favorite == true) {
+            button.text = "Добавить из избранного"
+            cat.favorite = false
+            change.deleteFromRealm();
+        }
+        else {
+            button.text = "Удалить из избранного"
+            cat.favorite = true
+            realm.copyToRealm(cat)
+        }
         realm.commitTransaction()
     }
 
-    private fun setText() {
+    private fun setText(button: Button) {
         val text = intent?.extras?.getString(CAT_FACT_TEXT_TAG)
+
+        val realm = Realm.getDefaultInstance()
+        realm.beginTransaction()
+        var change = realm.where(Cat::class.java).equalTo("text", text as String).findFirst()
+        if (change?.favorite == true) {
+            button.text = "Удалить из избранного"
+        }
+        else {
+            button.text = "Добавить в избранное"
+        }
+        realm.commitTransaction()
 
         val textView = findViewById<TextView>(R.id.DetailTextViewId)
         textView.text = text
